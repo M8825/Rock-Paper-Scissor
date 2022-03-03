9dd4b68feadf00fb9aac8b70186f62c7ac18d75b
MkMula <maho.mamulashvili@gmail.com>
2022-03-03 18:49:38 -0500

    Add user game options
    User can provide with game options, otherwise it will assign default game options

--- Rock-Paper-Scissors/task/game.py
+++ Rock-Paper-Scissors/task/game.py
@@ -1,21 +1,28 @@
 from random import choice
 
+
 user_name = input('Enter your name:')
 print(f'Hello, {user_name}')
 
 file = open('rating.txt', 'r')
-
 score = 0
 
 for line in file:
     name = line.split()
     if name[0] == user_name:
         score = int(name[1])
+        break
+file.close()
 
+# Get new game options list from user
+game_options = input()
+if not game_options:  # Assign default options
+    game_options = ['rock', 'paper', 'scissors']
+else:
+    game_options = game_options.split(',')
+
+print("Okay, let's start")
 while True:
-    comp_choice = choice(['rock', 'paper', 'scissors'])
-    comp_win_opts = {'rock': 'paper', 'scissors': 'rock', 'paper': 'scissors'}
-
     usr_inp = input()
 
     if usr_inp == '!exit':
@@ -24,16 +31,19 @@ while True:
     elif usr_inp == '!rating':
         print(f'Your rating: {score}')
         continue
-    elif usr_inp not in comp_win_opts:
+    elif usr_inp not in game_options:
         print('Invalid input')
         continue
 
-    option = comp_win_opts[usr_inp]
+    comp_choice = choice(game_options)
+    user_opt = game_options.index(usr_inp)  # Get option index in original list
+    new_options = game_options[user_opt + 1:] + game_options[:user_opt]
+    winners = new_options[:int(len(new_options) / 2)]
 
     if comp_choice == usr_inp:
         print(f'There is a draw ({comp_choice})')
         score += 50
-    elif option == comp_choice:
+    elif comp_choice in winners:
         print(f'Sorry, but the computer chose {comp_choice}')
     else:
         print(f'Well done. The computer chose {comp_choice} and failed')
